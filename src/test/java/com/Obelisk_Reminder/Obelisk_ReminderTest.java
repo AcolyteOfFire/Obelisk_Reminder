dcee0761dad29fd85304ffc02791b0498f231983
AcolyteOfFire <114724687+AcolyteOfFire@users.noreply.github.com>
2024-07-30 18:12:33 -0800

    Merge remote-tracking branch 'origin/master'

--- src/test/java/com/Obelisk_Reminder/Obelisk_ReminderTest.java
+++ src/test/java/com/Obelisk_Reminder/Obelisk_ReminderTest.java
@@ -3,7 +3,7 @@ package com.Obelisk_Reminder;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class Obelisk_ReminderTest
 {
 	public static void main(String[] args) throws Exception
 	{
